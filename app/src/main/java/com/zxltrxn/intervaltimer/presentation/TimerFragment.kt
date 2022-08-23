cb596c435dcad9fda4a544e91bac6cb26b30209d
ZXLTRXN <madbastaz21@gmail.com>
2022-08-23 11:26:12 +0700

    removed action bar

--- app/src/main/java/com/zxltrxn/intervaltimer/presentation/TimerFragment.kt
+++ app/src/main/java/com/zxltrxn/intervaltimer/presentation/TimerFragment.kt
@@ -1,5 +1,6 @@
 package com.zxltrxn.intervaltimer.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,10 +8,11 @@ import com.zxltrxn.intervaltimer.R
 import com.zxltrxn.intervaltimer.databinding.TimerFragmentBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
 
-class TimerFragment: Fragment(R.layout.timer_fragment) {
+class TimerFragment : Fragment(R.layout.timer_fragment) {
     private val binding by viewBinding(TimerFragmentBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.remainingTime.text = "12:12"
     }
 }
