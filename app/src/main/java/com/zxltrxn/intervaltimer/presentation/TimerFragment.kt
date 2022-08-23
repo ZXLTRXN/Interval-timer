5a8f3a8c27020cb86c7c50e0dd159f05e8d4ea56
ZXLTRXN <madbastaz21@gmail.com>
2022-08-23 11:12:07 +0700

    added fragment with view binding to nav_graph

---
+++ app/src/main/java/com/zxltrxn/intervaltimer/presentation/TimerFragment.kt
@@ -0,0 +1,16 @@
+package com.zxltrxn.intervaltimer.presentation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.zxltrxn.intervaltimer.R
+import com.zxltrxn.intervaltimer.databinding.TimerFragmentBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
+
+class TimerFragment: Fragment(R.layout.timer_fragment) {
+    private val binding by viewBinding(TimerFragmentBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
